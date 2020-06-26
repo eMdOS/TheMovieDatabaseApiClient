894174549970c46f92a7a95d24f94a3a165f3ff0
eMdOS <emilio.ojeda.mendoza@gmail.com>
2020-06-26 17:51:59 -0500

    refactor: organize Endpoint extensions

---
+++ Sources/TheMovieDatabaseApiClient/Utilitites/Endpoint+Movies.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+extension Endpoint {
+    static func nowPlaying(page: Int? = nil) -> Self {
+        Endpoint(
+            path: "/3/movie/now_playing",
+            query: page
+                .map(URLQueryItem.page)
+                .map({ [$0] })
+        )
+    }
+
+    static func popular(page: Int? = nil) -> Self {
+        Endpoint(
+            path: "/3/movie/popular",
+            query: page
+                .map(URLQueryItem.page)
+                .map({ [$0] })
+        )
+    }
+
+    static func topRated(page: Int? = nil) -> Self {
+        Endpoint(
+            path: "/3/movie/top_rated",
+            query: page
+                .map(URLQueryItem.page)
+                .map({ [$0] })
+        )
+    }
+
+    static func upcoming(page: Int? = nil) -> Self {
+        Endpoint(
+            path: "/3/movie/upcoming",
+            query: page
+                .map(URLQueryItem.page)
+                .map({ [$0] })
+        )
+    }
+
+    static func similar(movieID: Int, page: Int? = nil) -> Self {
+        Endpoint(
+            path: "/3/movie/\(movieID)/similar",
+            query: page
+                .map(URLQueryItem.page)
+                .map({ [$0] })
+        )
+    }
+
+    static func recommendations(movieID: Int, page: Int? = nil) -> Self {
+        Endpoint(
+            path: "/3/movie/\(movieID)/recommendations",
+            query: page
+                .map(URLQueryItem.page)
+                .map({ [$0] })
+        )
+    }
+
+    static func reviews(movieID: Int, page: Int? = nil) -> Self {
+        Endpoint(
+            path: "/3/movie/\(movieID)/reviews",
+            query: page
+                .map(URLQueryItem.page)
+                .map({ [$0] })
+        )
+    }
+
+    static func movie(id: Int) -> Self {
+        Endpoint(path: "/3/movie/\(id)")
+    }
+}
+
